146d6d8d97ab0ead51aa27a40578dfd4bcbb743e
JeanVictor0 <jeanvdsroque@gmail.com>
2022-08-18 15:45:07 -0400

    Adicionando base

---
+++ src/main/java/org/jeanvictor0/server_phone/ServerPhoneApplication.java
@@ -0,0 +1,13 @@
+package org.jeanvictor0.server_phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jeanvictor0/server_phone/ServerPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeanvictor0.server_phone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
